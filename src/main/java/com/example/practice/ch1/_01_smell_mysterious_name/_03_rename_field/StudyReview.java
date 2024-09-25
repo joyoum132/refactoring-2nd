abf5339aebabcfbb3c87d81c70a1ef7430a9fd4b
joyoum132 <yoojy01@gmail.com>
2024-09-26 00:42:59 +0900

    리팩토링3 - 필드 이름 변경(레코드 사용)

---
+++ src/main/java/com/example/practice/ch1/_01_smell_mysterious_name/_03_rename_field/StudyReview.java
@@ -0,0 +1,5 @@
+package com.example.practice.ch1._01_smell_mysterious_name._03_rename_field;
+
+//getter, setter, hashCode, equals 자동 생성
+public record StudyReview(String reviewer, String review) {
+}
