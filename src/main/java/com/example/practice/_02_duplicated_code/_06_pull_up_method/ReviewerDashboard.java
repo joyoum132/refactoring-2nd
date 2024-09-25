6965abc1e6f86ec1d3b5f7acf2f43de1d3055d2c
joyoum132 <yoojy01@gmail.com>
2024-09-26 01:50:57 +0900

    리팩토링6 - 메소드 올리기

--- src/main/java/com/example/practice/_02_duplicated_code/_06_pull_up_method/ReviewerDashboard.java
+++ src/main/java/com/example/practice/_02_duplicated_code/_06_pull_up_method/ReviewerDashboard.java
@@ -12,16 +12,7 @@ public class ReviewerDashboard extends Dashboard {
 
     public void printReviewers() throws IOException {
         // Get github issue to check homework
-        Set<String> reviewers = new HashSet<>();
-        GitHub gitHub = GitHub.connect();
-        GHRepository repository = gitHub.getRepository("whiteship/live-study");
-        GHIssue issue = repository.getIssue(30);
-
-        // Get reviewers
-        issue.getComments().forEach(c -> reviewers.add(c.getUserName()));
-
-        // Print reviewers
-        reviewers.forEach(System.out::println);
+        printUsernames(30);
     }
 
 }
