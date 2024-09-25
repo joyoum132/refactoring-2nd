7649b239861734c9da69bd2382783556b7cc1bf6
joyoum132 <yoojy01@gmail.com>
2024-09-26 07:55:09 +0900

    리팩토링9 - 객체를 통째로 넘기기

--- src/main/java/com/example/practice/_03_long_function/_09_preserve_whole_object/Participant.java
+++ src/main/java/com/example/practice/_03_long_function/_09_preserve_whole_object/Participant.java
@@ -4,6 +4,7 @@ package com.example.practice._03_long_function._09_preserve_whole_object;
 import java.util.HashMap;
 import java.util.Map;
 
+// Participant 레코드는 점차 클래스의 형태를 갖춰감
 public record Participant(String username, Map<Integer, Boolean> homework) {
     public Participant(String username) {
         this(username, new HashMap<>());
@@ -14,4 +15,10 @@ public record Participant(String username, Map<Integer, Boolean> homework) {
     }
 
 
+    double getRate(int totalNumberOfEvents) {
+        long count = homework().values().stream()
+                .filter(v -> v == true)
+                .count();
+        return (double) (count * 100 / totalNumberOfEvents);
+    }
 }
