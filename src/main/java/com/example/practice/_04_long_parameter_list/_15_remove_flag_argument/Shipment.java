a18ac94339c5190cf71cd6b5738a0e321e1725e2
joyoum132 <yoojy01@gmail.com>
2024-09-26 21:18:51 +0900

    리팩토링 15 - 플래그 인수 제거하기

--- src/main/java/com/example/practice/_04_long_parameter_list/_15_remove_flag_argument/Shipment.java
+++ src/main/java/com/example/practice/_04_long_parameter_list/_15_remove_flag_argument/Shipment.java
@@ -4,21 +4,30 @@ import java.time.LocalDate;
 
 public class Shipment {
 
+    //fixme 호출하는 곳에서 isRush 에 따라 각각 호출
     public LocalDate deliveryDate(Order order, boolean isRush) {
         if (isRush) {
-            int deliveryTime = switch (order.getDeliveryState()) {
-                case "WA", "CA", "OR" -> 1;
-                case "TX", "NY", "FL" -> 2;
-                default -> 3;
-            };
-            return order.getPlacedOn().plusDays(deliveryTime);
+            return rushDeliveryDate(order);
         } else {
-            int deliveryTime = switch (order.getDeliveryState()) {
-                case "WA", "CA" -> 2;
-                case "OR", "TX", "NY" -> 3;
-                default -> 4;
-            };
-            return order.getPlacedOn().plusDays(deliveryTime);
+            return regularDeliveryDate(order);
         }
+    }
+
+    public LocalDate regularDeliveryDate(Order order) {
+        int deliveryTime = switch (order.getDeliveryState()) {
+            case "WA", "CA" -> 2;
+            case "OR", "TX", "NY" -> 3;
+            default -> 4;
+        };
+        return order.getPlacedOn().plusDays(deliveryTime);
+    }
+
+    public LocalDate rushDeliveryDate(Order order) {
+        int deliveryTime = switch (order.getDeliveryState()) {
+            case "WA", "CA", "OR" -> 1;
+            case "TX", "NY", "FL" -> 2;
+            default -> 3;
+        };
+        return order.getPlacedOn().plusDays(deliveryTime);
     }
 }
