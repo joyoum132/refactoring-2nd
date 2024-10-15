bb7d7566203343bdb84c61aa5a6609d0470f2851
joyoum132 <yoojy01@gmail.com>
2024-10-15 22:57:32 +0900

    copy

---
+++ src/main/java/com/example/practice/_21_alternative_classes_with_different_interfaces/AlertMessage.java
@@ -0,0 +1,12 @@
+package com.example.practice._21_alternative_classes_with_different_interfaces;
+
+public class AlertMessage {
+
+    public void setMessage(String message) {
+
+    }
+
+    public void setFor(String email) {
+
+    }
+}
---
+++ src/main/java/com/example/practice/_21_alternative_classes_with_different_interfaces/AlertService.java
@@ -0,0 +1,5 @@
+package com.example.practice._21_alternative_classes_with_different_interfaces;
+
+public interface AlertService {
+    void add(AlertMessage alertMessage);
+}
